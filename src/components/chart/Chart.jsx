1b34dea21a612a1b9e77a7a82ee959ed4f463fa5
idyWilliams <williamsidorenyin@yahoo.com>
2022-09-22 22:19:24 +0100

    feat: added chat to dashboard ui

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
 import "./chart.scss"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area } from 'recharts';
+
+
+const data = [
+  {
+    name: 'Monday',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Tuesday',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Wednesday',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Thursday',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Friday',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Saturday',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Sunday',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 
 const Chart = () => {
   return (
-    <div className='chart'>Chart</div>
+    <div className='chart'>
+      <div className="title">weekly Revenue</div>
+      <ResponsiveContainer width="100%" height="100%">
+      <AreaChart width={730} height={250} data={data}
+  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  <defs>
+    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+    </linearGradient>
+    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
+  <XAxis dataKey="name" />
+  <YAxis />
+  <CartesianGrid strokeDasharray="3 3" />
+  <Tooltip />
+  <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+  <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+</AreaChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
-import "./featured.scss"
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import React from "react";
+import "./featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Featured = () => {
   return (
-    <div className='featured'>
-        <div className="top">
-            <h1 className='title'>Total Revenue</h1>
-            <MoreVertIcon fontSize='small'/>
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={50} text={"30%"} strokeWidth={5} />
         </div>
-        <div className="bottom">
-            <div className="featuredChat">
-<CircularProgressbar/>
+        <div className="title">Total sales</div>
+        <div className="amount">$420</div>
+        <p className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+          molestias.
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle ">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
             </div>
-            <div className="title">Total sales</div>
-            <div className="amount"></div>
+          </div>
+          <div className="item">
+            <div className="itemTitle ">Last Week</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle ">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
